7b059b0d06c6ec72d835297e6853c38a77fbd540
qyinm <qusseun@gmail.com>
2026-06-10 19:07:15 +0900

    feat(landing): scaffold Astro project in monorepo

---
+++ apps/landing/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://usehem.com',
+  integrations: [mdx(), react(), sitemap()],
+  output: 'static',
+  build: {
+    assets: '_assets',
+  },
+});
