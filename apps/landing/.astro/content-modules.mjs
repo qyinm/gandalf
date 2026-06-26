ebdf7c96e7c6dd3eac5757d8c3e7438e2604f8ca
qyinm <qusseun@gmail.com>
2026-06-26 22:33:39 +0900

    chore: ignore astro cache and align package version

--- apps/landing/.astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/docs/bundle-format.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbundle-format.mdx&astroContentModuleFlag=true")]]);
-		
